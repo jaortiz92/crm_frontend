7bf16222be6d2bbfe9009450402bb729b40f2477
John Ortiz <jaortiz92@hotmail.com>
2026-05-13 16:00:13 -0500

    Added config to use a new line Perssam

--- src/components/order/OrderForm.vue
+++ src/components/order/OrderForm.vue
@@ -218,7 +218,7 @@ const handleFileUpload = (event) => {
           <label>Marca:</label>
           <select v-model="order.type_format" required>
             <option key="1" value="kyly">Kyly</option>
-            <option key="2" value="dame">Givec o Tinta</option>
+            <option key="2" value="dame">Givec, Tinta o Perssam</option>
             <option key="3" value="pampili">Pampili</option>
           </select>
           <label for="document">Documento:</label>
